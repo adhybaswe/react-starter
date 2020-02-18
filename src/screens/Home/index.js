801defeae16364295dc60721caf6d2b01429fb23
cahsibat <cah.sibat@gmail.com>
2020-02-18 20:52:52 +0700

    Add screen Home

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clink from '../../components/Clink'
+
+const Home = (props) => {
+	return(
+		<div className="page-home">
+			<h1>Selamat Datang di Home</h1>
+			<Clink link="about" label="Go to about" type="primary" />
+		</div>
+	)
+}
+
+export default Home
