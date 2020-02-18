5f29bedfb9b74f70e470b7263b535c30de54b11e
cahsibat <cah.sibat@gmail.com>
2020-02-18 20:53:06 +0700

    Add screen About

---
+++ src/screens/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clink from '../../components/Clink'
+
+const About = (props) => {
+	return(
+		<div className="page-about">
+			<h1>Selamat Datang di About</h1>
+			<Clink link="/" label="Go to home" type="primary" />
+		</div>
+	)
+}
+
+export default About
