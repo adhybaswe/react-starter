640865781f087f1ec6ae41264cd5790713835588
cahsibat <cah.sibat@gmail.com>
2020-02-18 20:52:08 +0700

    Add component Clink

---
+++ src/components/Clink/Clink.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CLink from 'index'
+
+export default {
+  title: 'CLink',
+  component: CLink,
+};
+
+export const Default = () => (
+  <CLink link="#" type="primary" label="Button" />
+);
